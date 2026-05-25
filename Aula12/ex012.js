9a2f038e480f851046e06ebdd819795eaf72629b
Leonardo Oliveira <leo.gui1974@gmail.com>
2026-05-25 14:39:34 -0300

    if else e switch case

---
+++ Aula12/ex012.js
@@ -0,0 +1,12 @@
+var idade = 68
+
+if (idade < 16){
+    console.log("Não Vota")
+} else if (idade >= 16 && idade < 18){
+  
+        console.log("Voto Opcional")
+} else if (idade >= 18 && idade < 65){
+        console.log("Voto Obrigatório")
+} else{
+    console.log("Voto Opcional")
+}
