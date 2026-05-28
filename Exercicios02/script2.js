cd6e12ecab9b08f4ff4156b4214449d822bd14c8
Leonardo Oliveira <leo.gui1974@gmail.com>
2026-05-28 11:57:09 -0300

    exercicios

--- Exercicios02/script2.js
+++ Exercicios02/script2.js
@@ -2,7 +2,13 @@ function criar(){
     var num = parseInt(document.querySelector('#numero').value)
     var tabuada = document.querySelector('#tabuada')
 
+    if(num == 0 || isNaN(num)){
+        alert('Por favor, digite um número!')
+        return
+    } else {
+    tabuada.innerHTML = ''
     for(var i = 0; i <=12; i++){
         tabuada.innerHTML += `<option>${num} x ${i} = ${num*i}</option>`
     }
+  }
 }
